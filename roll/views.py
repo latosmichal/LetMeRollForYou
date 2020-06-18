50ce01b9d3e915fd915ab465b4863bb22c4b513e
latosmichal <michalt95@gmail.com>
2020-06-18 21:34:03 +0200

    First primitive rolling button

--- roll/views.py
+++ roll/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+import random
 
-def roll_main(request):
+def roll_main(request):    
     return render(request, 'roll/roll_main.html', {})
+
+
+def get_roll(request):    
+    roll = random.randint(1,6)
+    context = {}
+    context['roll'] = roll
+    return render(request, 'roll/roll_main.html', context)
