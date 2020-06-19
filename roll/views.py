566890b643f38ec0f31093b27eca9a25f32b57e3
pracowniaPK <kaczory5@gmail.com>
2020-06-19 20:17:29 +0200

    plug random into view

--- roll/views.py
+++ roll/views.py
@@ -7,10 +7,8 @@ def roll_main(request):
     if request.method == 'GET':
         k = request.GET.get('k')
         if k:
-            print(k)
-            print(get_roll_from_random(k=k, n=1)[0])
-            # wziąć wynik i wcisnąć w template
-            return render(request, 'roll/roll_main.html', {})
+            roll=get_roll_from_random(k=k, n=1)[0]
+            return render(request, 'roll/roll_main.html', {'roll':roll})
         else:
             return render(request, 'roll/roll_main.html', {})
 
